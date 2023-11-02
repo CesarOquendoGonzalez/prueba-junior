c778ee2f9c2a8f86d0fc46cbc4553f29ccb2f746
CesarOquendoGonzalez <118139089+CesarOquendoGonzalez@users.noreply.github.com>
2023-11-02 12:59:56 -0500

    Update HomePage.java

--- src/main/java/co/com/pragma/userinterfaces/home/HomePage.java
+++ src/main/java/co/com/pragma/userinterfaces/home/HomePage.java
@@ -6,7 +6,7 @@ import net.serenitybdd.core.annotations.findby.By;
 
 public class HomePage extends PageObject {
     public static final Target BTN_MY_ACCOUNT = Target.the("Boton para ingresar al desplegable donde puede iniciar sesion o registrarme").
-            located(By.xpath("//span[contains(@class, 'hidden-xs') and contains(@class, 'hidden-sm') and contains(@class, 'hidden-md')][text()='My Account']\n"));
+            located(By.xpath("/html/body/nav/div/div[2]/ul/li[2]/a/span[1]"));
     public static final Target BTN_LOGIN= Target.the("Boton para ingresar al login").
             located(By.xpath("/html/body/nav/div/div[2]/ul/li[2]/ul/li[2]/a"));
 
